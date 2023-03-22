69b3d6a5dee9c8bd26d29cf04567dfbf66be061a
FilippoFicarra <82835577+FilippoFicarra@users.noreply.github.com>
2023-03-22 12:30:37 +0100

    M1 writeup complete

--- Challenges/Week 4-graded/M1/writeup.py
+++ Challenges/Week 4-graded/M1/writeup.py
@@ -17,7 +17,16 @@ def json_send(req):
 
 
 def solve():
-
+    """
+    The fundamental request is the register one. We aim to register a user as an admin.
+    In orther to do so I exploited the fact that the server has a certain syntax for storing the token so that,
+    when decryptying it will be parsed as desired.
+    The principal part were:
+        - all the different fields should be a block long
+        - we should be able to inject the role=admin field in the username so that it is before 
+            the one added by the server
+    With this two conditions, we can be sure that role=admin is parsed berfore role=user and then we are registered as admin.
+    """
     request = {
         'command' : 'register',
         'username' : '0000000username=000000000000&role=admin',
