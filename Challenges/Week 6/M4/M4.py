e0e7bf30578c84a1406700ba408965d9b3f1b76a
FilippoFicarra <82835577+FilippoFicarra@users.noreply.github.com>
2023-05-04 18:23:02 +0200

    M4

--- Challenges/Week 6/M4/M4.py
+++ Challenges/Week 6/M4/M4.py
@@ -3,7 +3,7 @@
 import telnetlib
 import json
 import itertools
-from string import ascii_letters, digits
+from string import ascii_letters, digits, punctuation
 from Crypto.Hash import HMAC, SHA256
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
@@ -11,7 +11,7 @@ from Crypto.Util.Padding import pad
 
 
 
-ALPHABET = ascii_letters + digits
+ALPHABET = ascii_letters + digits + punctuation
 
 
 tn = telnetlib.Telnet("aclabs.ethz.ch", 50604)
@@ -28,6 +28,8 @@ def json_send(req):
     request = json.dumps(req).encode()
     tn.write(request + b"\n")
 
+def byte_xor(ba1, ba2):
+    return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
 
 def blockify(data: bytes, blocksize: int):
     assert(len(data) % blocksize == 0)
@@ -43,39 +45,84 @@ def solve():
     json_send(request)
     response = json_recv()
 
-    ctxt = response["ctxt"]
-    nonce = response["nonce"]
-    mac_tag = response["mac_tag"]
+    ctxt_f = response["ctxt"]
+    nonce_f = response["nonce"]
+    mac_tag_f = response["mac_tag"]
 
-    print(ctxt)
-    print(nonce)
-    print(mac_tag)
-
-
-    request = {
-        "command": "encrypt",
-        "ptxt": "A" * (len(ctxt)//2)
-    }
-    json_send(request)
-    response = json_recv()
-
-    ctxt = response["enc_flag"]
-    nonce = response["nonce"]
-    mac_tag = response["mac_tag"]
-
-    print(ctxt)
-    print(nonce)
-    print(mac_tag)
-
-
-
-
-
-
-
-
+    print(ctxt_f)
+    print(nonce_f)
+    print(mac_tag_f)
 
     
+
+    flag = ''
+    for i in range(0, len(ctxt_f)//2):
+        letter_tag = {}
+
+        for letter in ALPHABET:
+            request = {
+                "command": "encrypt",
+                "ptxt": flag + letter
+            }
+            json_send(request)
+            response = json_recv()
+
+            ctxt = response["enc_flag"]
+            nonce = response["nonce"]
+            mac_tag = response["mac_tag"]
+
+            letter_tag[flag + letter] = {
+                    "nonce" : nonce,
+                    "mac_tag" : mac_tag,
+                    "ctxt" : ctxt
+                }
+            
+        for keys in list(letter_tag.keys()):
+            print(keys)
+            request = {
+                "command": "decrypt",
+                "ctxt": ctxt_f[:(i+1)*2],
+                "nonce": nonce_f,
+                "mac_tag": letter_tag[keys]["mac_tag"]
+            }
+            json_send(request)
+            response = json_recv()
+
+            if response["success"] == True:
+                flag = keys
+                break
+        
+    print(flag)
+
+
+    # message = "A" * (len(ctxt_f)//2)
+    # request = {
+    #     "command": "encrypt",
+    #     "ptxt": message
+    # }
+    # json_send(request)
+    # response = json_recv()
+
+    # ctxt = response["enc_flag"]
+    # nonce = response["nonce"]
+    # mac_tag = response["mac_tag"]
+
+    # r = byte_xor(message.encode(), bytes.fromhex(ctxt))
+    # for i in range(2**12):
+    #     request = {
+    #         "command": "decrypt",
+    #         "ctxt": i.to_bytes(len(ctxt)//2, "big").hex(),# find the right ciphertext with associated nonce to encrypt in the ptxt associated to the mac, then retrieve through r
+    #         "nonce": nonce,
+    #         "mac_tag": mac_tag
+    #     }
+    #     json_send(request)
+    #     response = json_recv()
+
+
+    #     if response["success"] == True:
+    #         print("success")
+    #         ptxt = byte_xor(i.to_bytes(len(ctxt)//2, "big"), r).decode()
+    #         return ptxt
 
 
 if __name__ == "__main__":
