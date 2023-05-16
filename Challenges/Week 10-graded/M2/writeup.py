4359e952fa57c15ed16c0dd9d09a0e6c7f184e7f
FilippoFicarra <82835577+FilippoFicarra@users.noreply.github.com>
2023-05-16 14:46:33 +0200

    writeup M2 ready

--- Challenges/Week 10-graded/M2/writeup.py
+++ Challenges/Week 10-graded/M2/writeup.py
@@ -1,15 +1,12 @@
-import math
-import random
 import telnetlib
 import json
 from Crypto.Cipher import AES
 from Crypto.Protocol.KDF import HKDF
-
+from Crypto.Hash import SHA256
+from Crypto.Util import number
 
 server = "aclabs.ethz.ch"
 tn = telnetlib.Telnet(server, 51002)
-from Crypto.Hash import MD5, HMAC, SHA256
-from Crypto.Util import number
 
 
 def readline():
@@ -26,9 +23,7 @@ def json_send(req):
 def byte_xor(ba1, ba2):
     return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
 
-
-
-def find_exponent(g, h, p, q=20):
+def find_exponent(g, h, p, q):
     """
     Find the exponent x such that g^x = h (mod p).
     """
@@ -40,15 +35,16 @@ def find_exponent(g, h, p, q=20):
 
 
 def solve():
+    # I fix an upperbound for the order of the subgroup
     q = 1001
     while True:
         if q < 1000: 
             if (p-1)%q == 0:
                 g = pow(h, (p-1)//q, p) #find a g such that we have small subgroup and we can bruteforce to solve the discrete log
-                if g != 1 and g != 0 and g != p-1:
+                if g != 1 and g != 0 and g != p-1: #check if g is not trivial
                     break
             q = q + 1
-        else:
+        else: # if we can not find out a small subgroup, we generate a new prime number
             q = 2
             p = number.getPrime(1024)
             h = number.getRandomRange(1, p-1)
@@ -74,10 +70,11 @@ def solve():
     ciphertext = response["ciphertext"]
     tag = response["tag"]
     nonce = response["nonce"]
-
+    
+    # since q is small we can bruteforce to find the exponent
     sk = find_exponent(g, pk, p, q)
 
-
+    # we just need to compute the shared key
     shared = pow(bob_pubkey, sk, p)
     shared_bytes = shared.to_bytes(512, "big")
     pk_bytes = pk.to_bytes(512, "big")
